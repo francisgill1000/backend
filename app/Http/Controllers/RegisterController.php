d7c324c3e8bd5c1c47bccd06a7bce961eb63994d
codestudiodevs <143318942+codestudiodevs@users.noreply.github.com>
2023-12-10 01:44:43 +0400

    Update RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,6 +14,7 @@ class RegisterController extends Controller
         $data = $request->except("password_confirmation");
         $data["name"] = "ignore";
         $data["email"] = date("Y-m-d H:i:s");
+        $data["password"] = Hash::make($data["password"]);
         return User::create($data);
     }
 }
